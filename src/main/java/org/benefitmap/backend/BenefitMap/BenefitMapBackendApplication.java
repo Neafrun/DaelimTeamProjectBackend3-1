1413c9e4d1128e9f9e1bd950b8d74edc98349bd0
Arund793 <genesis793@naver.com>
2025-09-11 10:47:39 +0900

    chore: initial Spring Boot project

---
+++ src/main/java/org/benefitmap/backend/BenefitMap/BenefitMapBackendApplication.java
@@ -0,0 +1,13 @@
+package org.benefitmap.backend.BenefitMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitMapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitMapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/benefitmap/backend/BenefitMap/BenefitMapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.benefitmap.backend.BenefitMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitMapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
